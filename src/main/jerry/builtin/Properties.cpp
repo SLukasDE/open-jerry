5af68d051d488128d55e4ad91233d5658fc86822
Sven Lukas <Sven.Lukas@googlemail.com>
2021-06-27 11:21:52 +0200

    Added support for using environment variables in settings. Improved request handler short cut: always make automatic context and settings object if update object implementation has been specified or implementation is available as object type or parameters have been specified. If no object implementation is available but parameters have been specified then fall back to make an object of type esl::object::Properies

---
+++ src/main/jerry/builtin/Properties.cpp
@@ -0,0 +1,30 @@
+/*
+ * This file is part of Jerry application server.
+ * Copyright (C) 2020-2021 Sven Lukas
+ *
+ * Jerry is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Jerry is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with Jerry.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <jerry/builtin/Properties.h>
+#include <esl/object/Properties.h>
+
+namespace jerry {
+namespace builtin {
+
+std::unique_ptr<esl::object::Interface::Object> Properties::createSettings(const esl::object::Interface::Settings& settings) {
+	return std::unique_ptr<esl::object::Interface::Object>(new esl::object::Properties(settings));
+}
+
+} /* namespace builtin */
+} /* namespace jerry */
