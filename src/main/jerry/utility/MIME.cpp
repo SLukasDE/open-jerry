88be77a659a44525a613486c2a5dd5ffaae733d9
Sven Lukas <Sven.Lukas@googlemail.com>
2021-08-22 23:14:14 +0200

    refactored code a little bit and added function to load mime types from file

--- src/main/jerry/utility/MIME.cpp
+++ src/main/jerry/utility/MIME.cpp
@@ -17,10 +17,111 @@
  */
 
 #include <jerry/utility/MIME.h>
-#include <algorithm>
+
+#include <esl/utility/String.h>
+
+#include <map>
+#include <fstream>
+
+/* needed by split */
+#include <set>
+#include <vector>
 
 namespace jerry {
 namespace utility {
+namespace {
+
+std::map<std::string, std::string>& getExtensionToMimeType() {
+	static std::map<std::string, std::string> extensionToMimeType = {
+		{"bmp", "image/bmp"},
+		{"bz", "application/x-bzip"},
+		{"bz2", "application/x-bzip2"},
+		{"css", "text/css"},
+		{"gif", "image/gif"},
+		{"gz", "application/x-compressed"},
+		{"gzip", "application/x-gzip"},
+		{"htm", "text/html"},
+		{"html", "text/html"},
+		{"jpg", "image/jpeg"},
+		{"jpeg", "image/jpeg"},
+		{"js", "text/javascript"},
+		{"log", "text/plain"},
+		{"mp3", "audio/mpeg3"},
+		{"png", "image/png"},
+		{"text", "text/plain"},
+		{"txt", "text/plain"},
+		{"tgz", "application/x-compressed"},
+		{"tif", "image/tiff"},
+		{"wav", "audio/wav"},
+		{"woff", "font/woff"},
+		{"woff2", "font/woff2"},
+		{"xls", "application/excel"},
+		{"xml", "text/xml"},
+		{"zip", "application/zip"}
+	};
+
+	return extensionToMimeType;
+}
+
+std::vector<std::string> split(const std::string& str, std::set<const char> separators, bool dropEmptyContent = false) {
+    std::vector<std::string> rv;
+    std::string::const_iterator currentIt = str.begin();
+    std::string::const_iterator lastIt = currentIt;
+
+    for(;currentIt != str.end(); ++currentIt) {
+        if(separators.count(*currentIt) > 0) {
+        	std::string content(lastIt, currentIt);
+        	if(dropEmptyContent == false || content.empty() == false) {
+                rv.push_back(content);
+        	}
+
+            lastIt = currentIt;
+            ++lastIt;
+        }
+    }
+
+    std::string content(lastIt, currentIt);
+	if(dropEmptyContent == false || content.empty() == false) {
+        rv.push_back(content);
+	}
+
+    return rv;
+}
+
+} /* anonymous namespace */
+
+void MIME::loadDefinition(const std::string& filename) {
+	std::map<std::string, std::vector<std::string>> mimeTypeToExtensions = {};
+	std::ifstream infile(filename);
+
+	std::string line;
+	while (std::getline(infile, line)) {
+		auto columns = split(line, {{' '}, {'\t'}}, true);
+		for(auto iter = columns.begin(); iter != columns.end(); ++iter) {
+			if(iter->empty() || iter->at(0) == '#') {
+				columns.erase(iter, columns.end());
+				break;
+			}
+		}
+
+		if(columns.empty()) {
+			continue;
+		}
+
+		std::map<std::string, std::string>& extensionToMimeType = getExtensionToMimeType();
+
+		if(columns[0] == "-") {
+			for(const std::size_t index = 1; index < columns.size(); ++index) {
+				extensionToMimeType.erase(columns[index]);
+			}
+		}
+		else {
+			for(const std::size_t index = 1; index < columns.size(); ++index) {
+				extensionToMimeType[columns[index]] = columns[0];
+			}
+		}
+	}
+}
 
 esl::utility::MIME MIME::byFilename(const std::string& filename) {
     std::size_t pos = filename.find_last_of(".");
@@ -35,79 +136,21 @@ esl::utility::MIME MIME::byFilename(const std::string& filename) {
 }
 
 esl::utility::MIME MIME::byFileExtension(std::string fileExtension) {
-    std::transform(fileExtension.begin(), fileExtension.end(), fileExtension.begin(), tolower);
+    const std::map<std::string, std::string>& extensionToMimeType = getExtensionToMimeType();
 
-    if(fileExtension == "bmp") {
-        return esl::utility::MIME("image/bmp");
-    }
-    else if(fileExtension == "bz") {
-        return esl::utility::MIME("application/x-bzip");
-    }
-    else if(fileExtension == "bz2") {
-        return esl::utility::MIME("application/x-bzip2");
-    }
-    else if(fileExtension == "css") {
-        return esl::utility::MIME("text/css");
-    }
-    else if(fileExtension == "gif") {
-        return esl::utility::MIME("image/gif");
-    }
-    else if(fileExtension == "gz") {
-        return esl::utility::MIME("application/x-compressed");
-    }
-    else if(fileExtension == "gzip") {
-        return esl::utility::MIME("application/x-gzip");
-    }
-    else if(fileExtension == "htm" || fileExtension == "html") {
-        return esl::utility::MIME("text/html");
-    }
-    else if(fileExtension == "jpg" || fileExtension == "jpeg") {
-        return esl::utility::MIME("image/jpeg");
-    }
-    else if(fileExtension == "js") {
-        return esl::utility::MIME("text/javascript");
-    }
-    else if(fileExtension == "log") {
-        return esl::utility::MIME(esl::utility::MIME::textPlain);
-    }
-    else if(fileExtension == "mp3") {
-        return esl::utility::MIME("audio/mpeg3");
-    }
-    else if(fileExtension == "png") {
-        return esl::utility::MIME("image/png");
-    }
-    else if(fileExtension == "text") {
-        return esl::utility::MIME(esl::utility::MIME::textPlain);
-    }
-    else if(fileExtension == "txt") {
-        return esl::utility::MIME(esl::utility::MIME::textPlain);
-    }
-    else if(fileExtension == "tgz") {
-        return esl::utility::MIME("application/x-compressed");
-    }
-    else if(fileExtension == "tif") {
-        return esl::utility::MIME("image/tiff");
-    }
-    else if(fileExtension == "wav") {
-        return esl::utility::MIME("audio/wav");
-    }
-    else if(fileExtension == "woff") {
-        return esl::utility::MIME("font/woff");
-    }
-    else if(fileExtension == "woff2") {
-        return esl::utility::MIME("font/woff2");
-    }
-    else if(fileExtension == "xls") {
-        return esl::utility::MIME("application/excel");
-    }
-    else if(fileExtension == "xml") {
-        return esl::utility::MIME(esl::utility::MIME::textXml);
-    }
-    else if(fileExtension == "zip") {
-        return esl::utility::MIME("application/zip");
+    auto iter = extensionToMimeType.find(fileExtension);
+    if(iter != std::end(extensionToMimeType)) {
+        return esl::utility::MIME(iter->second);
     }
 
-    return esl::utility::MIME(esl::utility::MIME::textPlain);
+    //std::transform(fileExtension.begin(), fileExtension.end(), fileExtension.begin(), tolower);
+    fileExtension = esl::utility::String::toLower(std::move(fileExtension));
+    iter = extensionToMimeType.find(fileExtension);
+    if(iter != std::end(extensionToMimeType)) {
+        return esl::utility::MIME(iter->second);
+    }
+
+    return esl::utility::MIME();
 }
 
 } /* namespace utility */
